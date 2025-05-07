e5640953793dcbef20aeb167c8c3ef1073c207df
totorod1120 <totorodriguez1120@gmail.com>
2025-05-07 10:02:59 -0300

    Arregle el problema con el percentil y cambie el histograma para que le haga un filtro gaussiano para detectar con mejor precision los maximos

--- analisis.py
+++ analisis.py
@@ -6,11 +6,10 @@ from scipy.ndimage import uniform_filter
 from scipy.signal import find_peaks
 
 class ImageEnhancer:
-    def __init__(self, imagen, sigma_background=100, alpha=0, percentil_contornos=99.9):
+    def __init__(self, imagen, sigma_background=100, alpha=0):
         self.image = imagen
         self.sigma_background = sigma_background
         self.alpha = alpha
-        self.percentil_contornos = percentil_contornos
 
     def _subtract_background(self):
         background = gaussian(self.image.astype(np.float32), sigma=self.sigma_background, preserve_range=True)
@@ -18,7 +17,10 @@ class ImageEnhancer:
         return corrected
 
     def _detect_histogram_peaks(self, image, min_intensity=5, min_dist=30):
-        hist, bins = np.histogram(image[image > min_intensity], bins=256, range=(0, 255))
+        histograma, bins = np.histogram(image[image > min_intensity], bins=256, range=(0, 255))
+        histograma[:5] = 0  # eliminar intensidades muy bajas
+        
+        hist = gaussian(histograma.astype(float), sigma=5)
         peaks, _ = find_peaks(hist, distance=min_dist)
         peak_vals = hist[peaks]
 
@@ -39,39 +41,45 @@ class ImageEnhancer:
         delta = corrected - centro
         return np.exp(-0.5 * (delta / sigma) ** 2) * delta
 
-    def _apply_tanh(self, image, ganancia=1):
-        delta = image - np.mean(image)
+    def _apply_tanh(self, image, ganancia=1, centro = 100):
+        delta = image - centro
         return 0.5 * (np.tanh(ganancia * delta) + 1)
 
-    def _find_large_contours(self, binary):
+    def _find_large_contours(self, binary,percentil_contornos=0):
         contours = find_contours(binary, level=0.5)
+        percentil = percentil_contornos
+    
+        if percentil > 0:
+            def area_contorno(contour):
+                x = contour[:, 1]
+                y = contour[:, 0]
+                return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+    
+            areas = np.array([area_contorno(c) for c in contours])
+            umbral = np.percentile(areas, percentil)
+            return [c for c, a in zip(contours, areas) if a >= umbral]
+    
+        return contours
 
-        def area_contorno(contour):
-            x = contour[:, 1]
-            y = contour[:, 0]
-            return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
-
-        areas = np.array([area_contorno(c) for c in contours])
-        umbral = np.percentile(areas, self.percentil_contornos)
-        return [c for c, a in zip(contours, areas) if a >= umbral]
-
-    def procesar(self, suavizado=5, ganancia_tanh=0.1, mostrar=True, percentil_contornos=None, min_dist_picos=30):
+    def procesar(self, suavizado=5, ganancia_tanh=0.1, mostrar=True, percentil_contornos=0, min_dist_picos=30):
         corrected = self._subtract_background()
-        centro, sigma, hist, top_peaks = self._detect_histogram_peaks(corrected)
+        centro, sigma, hist, top_peaks = self._detect_histogram_peaks(corrected,min_dist=min_dist_picos)
 
         enhanced = self._enhance_tanh_diff2(corrected, centro, sigma)
         enhanced_norm = (enhanced - enhanced.min()) / (enhanced.max() - enhanced.min())
         enhanced_uint8 = (enhanced_norm * 255).astype(np.uint8)
 
         smooth = uniform_filter(enhanced_uint8, size=suavizado)
-
-        enhanced2 = self._apply_tanh(smooth, ganancia=ganancia_tanh)
+        
+        centro1, sigma1, hist1, top_peaks1 = self._detect_histogram_peaks(smooth,min_dist=min_dist_picos)
+        
+        enhanced2 = self._apply_tanh(smooth, ganancia=ganancia_tanh, centro=centro1)
         enhanced2_norm = (enhanced2 - enhanced2.min()) / (enhanced2.max() - enhanced2.min())
         enhanced2_uint8 = (enhanced2_norm * 255).astype(np.uint8)
-
+        
         threshold = threshold_otsu(enhanced2_uint8)
         binary = (enhanced2_uint8 > threshold).astype(np.uint8) * 255
-        contornos = self._find_large_contours(binary)
+        contornos = self._find_large_contours(enhanced2_uint8,percentil_contornos)
 
         if mostrar:
             self._mostrar_resultados(enhanced_uint8, smooth, enhanced2_uint8, binary, contornos, hist, top_peaks)
@@ -116,6 +124,8 @@ class ImageEnhancer:
         plt.title("Histograma + Picos seleccionados")
         plt.xlabel("Intensidad")
         plt.ylabel("Frecuencia")
+        
+        print(f"Cantidad de contornos detectados: {len(contornos)}")
 
         plt.tight_layout()
         plt.show()
@@ -125,9 +135,15 @@ class ImageEnhancer:
 
 from skimage.io import imread
 
-imagen = imread("C:/Users/Tomas/Desktop/FACULTAD/LABO 6/2000.tif")
+#imagen = imread("C:/Users/Tomas/Desktop/FACULTAD/LABO 6/2000.tif")
+imagen = imread("C:/Users/Tomas/Desktop/FACULTAD/LABO 6/Resta-P8139-150Oe-50ms-1000.tif")[400:700, 475:825]
 enhancer = ImageEnhancer(imagen=imagen)
-binary, contornos = enhancer.procesar(ganancia_tanh=1, suavizado=5, percentil_contornos=0, min_dist_picos=20)
+binary, contornos = enhancer.procesar(
+    ganancia_tanh=0.1,
+    suavizado=3,
+    percentil_contornos=99.9,
+    min_dist_picos= 5
+)
 
 #%%
 
